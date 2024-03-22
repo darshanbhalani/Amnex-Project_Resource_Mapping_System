0d556869e96493983d66e234cf34f63f324dff36
DARSHAN BHALANI <darsganbhalanipatel@gmail.com>
2024-03-22 17:08:37 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Amnex_Project_Resource_Mapping_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
