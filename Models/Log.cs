7f853bb67916c596dade8cfcee4e1d49e10b06d7
DarshanBhalani-Amnex <33333gmmaaiil@gmail.com>
2024-04-24 18:47:49 +0530

    24/04/2024

---
+++ Models/Log.cs
@@ -0,0 +1,12 @@
+﻿namespace Amnex_Project_Resource_Mapping_System.Models
+{
+    public class Log
+    {
+        public string EntityName { get; set; }
+        public string EntityType { get; set; }
+        public string Description { get; set; }
+        public string Action { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string LogBy { get; set; }
+    }
+}
